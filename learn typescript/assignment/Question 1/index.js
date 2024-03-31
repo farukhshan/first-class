d021208c01cff721aa125c1c30241ffa22793cb5
farrukh shan <farrukhshan55@gmail.com>
2024-03-31 15:11:36 +0500

    initialize commit

---
+++ learn typescript/assignment/Question 1/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("hello world");
+let myName = "Hello";
+console.log(" ~ myName:", myName);
---
+++ learn typescript/assignment/Question 1/index.ts
@@ -0,0 +1,3 @@
+console.log("hello world");
+let myName = "Hello";
+console.log(" ~ myName:", myName);
